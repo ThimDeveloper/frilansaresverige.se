a7651d848a720aae58c89ce13de08d3529984746
Thim Lohse <thim.lohse@caesari.com>
2022-03-18 17:39:04 +0100

    feat(storybook): improving storybook support

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ const config = {
   extends: ['next/core-web-vitals', 'plugin:react-hooks/recommended'],
   plugins: ['testing-library'],
   env: {
-    jest: true,
+    jest: true
   },
-  overrides: [
-    // Only uses Testing Library lint rules in test files
-    {
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
-    },
-  ],
-}
-export default config
+  overrides: [// Only uses Testing Library lint rules in test files
+  {
+    files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+    extends: ['plugin:testing-library/react']
+  }]
+};
+export default config;
+export const extends = ["plugin:storybook/recommended"];
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   /** Expose public folder to storybook as static */
   staticDirs: ['../public'],
   addons: [
@@ -25,14 +28,14 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  webpackFinal: async (config) => {
+  webpackFinal: (config) => {
     /**
      * Add support for alias-imports
      * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
      */
     config.resolve.alias = {
       ...config.resolve?.alias,
-      '@': [path.resolve(__dirname, '../')],
+      '@': [path.resolve(__dirname, '../'), path.resolve(__dirname, '../')],
     }
 
     /**
